c41cc4304cef509ed4ca4a3dc7206ca1ba8e8a8b
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-06-04 23:20:51 +0000

    Update convert_model.py to correctly load and save model
    Update convert_model.py to correctly load .pt model, state_dict,
    and save as a directory with model safetensors

--- convert_model.py
+++ convert_model.py
@@ -1,28 +1,40 @@
 import torch
-from transformers import AutoModel
-from transformers import AutoTokenizer, AutoConfig
-from transformers import GPTNeoXForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, GPTNeoXForCausalLM
 
-# Define the path to the input model and the output file
-exp_dir = ""
 in_path = f"/root/policy_dcpo.pt/policy.pt"
 out_path = f"/root/policy_dcpo/"
 
+# Load the state dict
 state_dict = torch.load(in_path)
+
+print(state_dict.keys())
+# print(state_dict['state'].keys())
+
+# Load the model
 model = GPTNeoXForCausalLM.from_pretrained('EleutherAI/pythia-2.8b')
-model.load_state_dict(state_dict)
+
+# Update the model's state dict
+model.load_state_dict(state_dict["state"])
+
+
+print("Keys in the model's state_dict:")
+print(model.state_dict().keys())
+
+# Save the model
 model.save_pretrained(out_path)
 
+raise e
+
+
+# Load and save tokenizer and config
 tokenizer = AutoTokenizer.from_pretrained('EleutherAI/pythia-2.8b')
 config = AutoConfig.from_pretrained('EleutherAI/pythia-2.8b')
 
 tokenizer.save_pretrained(out_path)
 config.save_pretrained(out_path)
 
-
-
-# Test 
-model = AutoModel.from_pretrained(out_path)
+# Test
+model = GPTNeoXForCausalLM.from_pretrained(out_path)
 tokenizer = AutoTokenizer.from_pretrained(out_path)
 
 inputs = tokenizer("Hello, world!", return_tensors="pt")
