228c5432def763595ca3969517da80545156c10b
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-06-07 15:28:12 -0400

    Cleanup extra comments in convert_model.py

--- convert_model.py
+++ convert_model.py
@@ -6,28 +6,17 @@ from transformers import (
     GPTNeoXForCausalLM,
 )
 
+
 in_path = f"/root/policy_dcpo.pt/policy.pt"
 out_path = f"/root/policy_dcpo/"
 
-# Load the state dict
 state_dict = torch.load(in_path)
-
 print(state_dict.keys())
-# print(state_dict['state'].keys())
 
-# Load the model
 model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-2.8b")
-
-# Update the model's state dict
 model.load_state_dict(state_dict["state"])
-
-# print("Keys in the model's state_dict:")
-# print(model.state_dict().keys())
-
-# Save the model
 model.save_pretrained(out_path)
 
-# Load and save tokenizer and config
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-2.8b")
 config = AutoConfig.from_pretrained("EleutherAI/pythia-2.8b")
 
@@ -41,5 +30,6 @@ tokenizer = AutoTokenizer.from_pretrained(out_path)
 
 inputs = tokenizer("Hello, world!", return_tensors="pt")
 outputs = model(**inputs)
-
 print(outputs)
+
+
