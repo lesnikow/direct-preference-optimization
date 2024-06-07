3bfbe51e96bb63a6ef0811d1f2f37e62dc8096c7
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-06-07 15:29:19 -0400

    Update convert_model to have a main function and be importable

--- convert_model.py
+++ convert_model.py
@@ -6,30 +6,31 @@ from transformers import (
     GPTNeoXForCausalLM,
 )
 
+def main():
+    in_path = f"/root/policy_dcpo.pt/policy.pt"
+    out_path = f"/root/policy_dcpo/"
 
-in_path = f"/root/policy_dcpo.pt/policy.pt"
-out_path = f"/root/policy_dcpo/"
+    state_dict = torch.load(in_path)
+    print(state_dict.keys())
 
-state_dict = torch.load(in_path)
-print(state_dict.keys())
+    model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-2.8b")
+    model.load_state_dict(state_dict["state"])
+    model.save_pretrained(out_path)
 
-model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-2.8b")
-model.load_state_dict(state_dict["state"])
-model.save_pretrained(out_path)
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-2.8b")
+    config = AutoConfig.from_pretrained("EleutherAI/pythia-2.8b")
 
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-2.8b")
-config = AutoConfig.from_pretrained("EleutherAI/pythia-2.8b")
-
-tokenizer.save_pretrained(out_path)
-config.save_pretrained(out_path)
-
-
-# Test
-model = GPTNeoXForCausalLM.from_pretrained(out_path)
-tokenizer = AutoTokenizer.from_pretrained(out_path)
-
-inputs = tokenizer("Hello, world!", return_tensors="pt")
-outputs = model(**inputs)
-print(outputs)
+    tokenizer.save_pretrained(out_path)
+    config.save_pretrained(out_path)
 
 
+def test():
+    model = GPTNeoXForCausalLM.from_pretrained(out_path)
+    tokenizer = AutoTokenizer.from_pretrained(out_path)
+
+    inputs = tokenizer("Hello, world!", return_tensors="pt")
+    outputs = model(**inputs)
+    print(outputs)
+
+if __name__ == "__main__":
+    main()
