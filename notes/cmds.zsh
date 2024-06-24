e962822b32e43673cdc1ec862c2882916cda5e70
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-06-24 19:52:23 -0400

    Update fastchat setup for chai cluster

--- notes/cmds.zsh
+++ notes/cmds.zsh
@@ -38,9 +38,14 @@ wandb login $WANDB_API_KEY
 
 
 ### Evals via fast-chat setup
-cd ~
+cd /nas/ucb/adamlesnikowski
+python3 -m venv env-fastchat
+source env-fastchat/bin/activate
+pip install --upgrade pip
+
 git clone https://github.com/lesnikow/fast-chat.git fastchat
 cd fastchat
+
 pip3 install -e ".[model_worker,webui]"
 pip install anthropic openai==0.28
 
