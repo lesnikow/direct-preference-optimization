7990927e9b9efdef9e03193083055565da8f6acb
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-09-14 19:39:32 -0700

    Put cmds in 0_setup.sh functions for modularity

--- 0_setup.sh
+++ 0_setup.sh
@@ -2,15 +2,16 @@
 # This script is intended to setup a new machine for our DCPO project.
 
 
-
 function install_packages() {
     sudo apt install -y neovim htop atop bmon tree python3.10-venv zsh unzip
 }
+
 
 function install_omz() {
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     chsh -s $(which zsh)
 }
+
 
 function install_vim_plug() {
     mkdir -p .config/nvim
@@ -21,46 +22,128 @@ function install_vim_plug() {
 }
 
 
+function setup_env_dpo() {
+    python3.11 -m venv env-dpo
+    deactivate && source env-dpo/bin/activate
+    pip install --upgrade pip
+    echo "source env-dpo/bin/activate" >> ~/.bashrc 
+    deactivate
+}
+
+
+function setup_env_fastchat() {
+    cd $HOME
+    python3 -m venv env-fastchat
+    deactivate && source env-fastchat/bin/activate
+    pip install --upgrade pip
+    deactivate
+}
+
+
+function config_git() {
+    git config --global user.email "adam.lesnikowski@gmail.com"
+    git config --global user.name "Adam Lesnikowski"
+    git config --global credential.helper cache
+    git config --global credential.helper 'cache --timeout=360000'
+}
+
+
+function setup_llm_sct_repo() {
+    cd $HOME
+    git clone https://github.com/lesnikow/llm-sct.git
+}
+
+
+function setup_direct_preference_optimization_repo() {
+    cd $HOME
+    git clone https://github.com/lesnikow/direct-preference-optimization.git 
+    cd direct-preference-optimization
+    source env-dpo/bin/activate
+    pip install -r requirements.txt
+    deactivate && cd $HOME
+}
+
+
+function_setup_fastchat_repo() {
+    cd $HOME
+    git clone https://github.com/lesnikow/fast-chat.git
+    cd fast-chat
+    source env-fastchat/bin/activate
+    pip install -e ".[model_worker,webui,llm_judge]"
+    pip install anthropic openai==0.28
+    deactivate && cd $HOME
+}
+
+
+function wanddb_login() {
+    source $HOME/direct-preference-optimization/.env
+    wandb login $WANDB_API_KEY
+}
+
+
+function main() {
+    echo "Starting 0_setup.sh script..."
+
+    echo "Finished 0_setup.sh script."
+}
+
+main
 
 
 
 
-python3.11 -m venv env
-source env/bin/activate
-pip install --upgrade pip
-echo "source env/bin/activate" >> ~/.bashrc 
-echo "clear" >> ~/.bashrc
-
-git config --global user.email "adam.lesnikowski@gmail.com"
-git config --global user.name "Adam Lesnikowski"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=360000'
 
 
-### Data setup
-git clone https://github.com/lesnikow/llm-sct.git
 
 
-### DPO, SFT setup
-cd ~
-git clone https://github.com/lesnikow/direct-preference-optimization.git 
-cd direct-preference-optimization
-pip install -r requirements.txt
-
-# vim .env
-source .env
-wandb login $WANDB_API_KEY
 
 
-### Evals via fast-chat setup
-cd $HOME
-python3 -m venv env-fastchat
-deactivate && source $HOME/env-fastchat/bin/activate
-pip install --upgrade pip
 
-cd fast-chat
-pip3 install -e ".[model_worker,webui]"
-pip3 install -e ".[model_worker,webui,llm_judge]"
 
-pip install anthropic openai==0.28
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
