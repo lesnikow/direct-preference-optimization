c80acaae39258fc06775918cc8ce0d8976d6b95d
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-09-14 18:53:45 -0700

    Finish evals cmds for maj vs sc in notes/cmds.zsh

--- notes/cmds.zsh
+++ notes/cmds.zsh
@@ -142,6 +142,95 @@ run_dpo $dataset $exp_name $sft_exp_dir
 
 
 
+## DONE: Evals
+### A. Run model adapter code models
+### Get dpo_exp_dirs from dpo .cache/adamlesnikowski/ dir
+
+deactivate && source $HOME/env/bin/activate
+cd $HOME/direct-preference-optimization/
+dpo_exp_dirs=(
+  "shp_maj_data_dataset_dpo_loss_pythia28_model_32_batch_size_2024-09-13_13-37-45_408534"
+  "shp_sc_data_dataset_dpo_loss_pythia28_model_32_batch_size_2024-09-13_13-37-59_889195"
+)
+
+for exp_dir in "${dpo_exp_dirs[@]}"; do
+  in_path="$HOME/direct-preference-optimization/.cache/adamlesnikowski/${exp_dir}/LATEST/policy.pt"
+  du -sh "${in_path}"
+  python3 convert_model.py --in_path ${in_path}
+done
+deactivate
+
+
+### B. Make fast-chat llm-judge answers
+
+deactivate && source $HOME/env-fastchat/bin/activate
+cd $HOME/fast-chat/fastchat/llm_judge/
+
+source $HOME/direct-preference-optimization/.env
+export OPENAI_API_KEY
+max_new_tokens=128
+
+
+function gen_model_answers {
+  exp_dir=$1
+  max_new_tokens=$2
+  model_path="$HOME/direct-preference-optimization/.cache/adamlesnikowski/${exp_dir}/LATEST/converted/"
+  echo "Model path: ${model_path}"
+  python3 gen_model_answer.py \
+    --model-path ${model_path} \
+    --model-id ${exp_dir} \
+    --num-gpus-total 4 \
+    --max-new-token ${max_new_tokens}
+}
+
+for exp_dir in "${dpo_exp_dirs[@]}"; do
+    echo "Generating model answers for ${exp_dir}"
+    gen_model_answers "${exp_dir}" "${max_new_tokens}"
+done
+
+
+### C. Make fast-chat llm-judge judgements
+
+python3 gen_judgment.py \
+  --mode "pairwise-all" \
+  --judge-model "gpt-4-turbo" \
+  --model-list "${dpo_exp_dirs[@]}" \
+  --parallel 256
+
+python3 gen_judgment.py \
+  --mode "single" \
+  --judge-model "gpt-4-turbo" \
+  --model-list "${dpo_exp_dirs[@]}" \
+  --parallel 256
+
+### D. Show results
+
+python3 show_result.py \
+  --mode "pairwise-all" \
+  --judge-model "gpt-4-turbo" \
+  --model-list "${dpo_exp_dirs[@]}"
+
+python3 show_result.py \
+  --mode "single" \
+  --judge-model "gpt-4-turbo" \
+  --model-list "${dpo_exp_dirs[@]}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ## WIP: RMP / AV for 33_all_voters
 
 ## SFT
