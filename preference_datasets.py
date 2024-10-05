5b70de7a890496b64f45b047105c95c84b4129f7
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-10-04 20:36:42 -0700

    Implelement get 160k maj, sc datasets in pref_datasets.py

--- preference_datasets.py
+++ preference_datasets.py
@@ -575,6 +575,23 @@ def get_shp_sc_data_v2_40k_rump_cut():
     return get_custom_shp_dataset_from_fp(fp, silent=False, verbose=False)
 
 
+
+def get_shp_maj_data_v2_160k():
+
+    home_dir = os.environ.get('HOME', '')
+    fp = os.path.join(home_dir,
+            "llm-sct/data/reddit/raw/gpt-3.5-turbo-0125/reddit_maj_data_for_DCPO_v2_160k.json")
+    return get_custom_shp_dataset_from_fp(fp, silent=False, verbose=False)
+   
+
+def get_shp_sc_data_v2_topic_matched_to_maj_160k():
+
+    home_dir = os.environ.get('HOME', '')
+    fp = os.path.join(home_dir,
+            "llm-sct/data/reddit/raw/gpt-3.5-turbo-0125/reddit_sc_data_for_DCPO_v2_topic_matched_to_maj_160k.json")
+    return get_custom_shp_dataset_from_fp(fp, silent=False, verbose=False)
+
+
 def get_dataset(name: str, split: str, silent: bool = False, cache_dir: str = None):
     """Load the given dataset by name. Supported by default are 'shp', 'hh', and 'se'."""
     if name == "":
