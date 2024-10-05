4731c8ce947cfbef83b96616fe585da727840fa6
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-10-05 15:11:19 -0700

    Update 1_sft.sh for shp_maj_data_v2_640k and sc datasets

--- 1_sft.sh
+++ 1_sft.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Soft fine-tuning (SFT) experiments
 
-a_arm_dataset='${HOME}/llm-sct/data/reddit/raw/gpt-3.5-turbo-0125/reddit_maj_data_for_DCPO_v2_160k.json'
-b_arm_dataset='${HOME}/llm-sct/data/reddit/raw/gpt-3.5-turbo-0125/reddit_sc_data_for_DCPO_v2_topic_matched_to_maj_160k.json'
+a_arm_dataset='shp_maj_data_v2_640k'
+b_arm_dataset='shp_sc_data_v2_topic_matched_to_maj_640k'
 n_arm_dataset='no_train_dataset'
 model='pythia69'
 model_fsdp_policy_mp="bfloat16"
