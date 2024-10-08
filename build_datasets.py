85ffd56e40ef13eb3ac2d4ff3fc5b805f86d2253
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-10-07 23:34:24 -0700

    Implement first pass of sc_dataset generation in build_datasets.py

--- build_datasets.py
+++ build_datasets.py
@@ -11,11 +11,55 @@ import sys
 import time
 
 
+def build_maj_data():
+    """Build the majority datasets."""
+
+    with open(fp_maj_all, "r") as f:
+        data_maj = f.readlines()
+        logging.info(f"Loaded {len(data_maj)} maj comments")
+
+
 def main():
     """Main function"""
 
     logging.info("Building datasets")
 
+    fp_maj_all = "/home/adam/data/reddit_data_v2/reddit_maj_data_for_DCPO_v2_all.json"
+    fp_sc_all = "/home/adam/data/reddit_data_v2/reddit_sc_data_for_DCPO_v2_all.json"
+
+    # Load the data, fp is actually a python lis of dictionaries.
+
+    with open(fp_sc_all, "r") as f:
+        data_sc = f.readlines()
+        logging.info(f"Loaded {len(data_sc)} sc comments")
+
+    # Build dict of completions, grouped by same prompt.
+    # Sample dictionary is form:
+    # {"prompt": "Prompt text ... ", "chosen": "Chosen completion text... ", "rejected":
+    # "rejected completion text..."}
+
+    sc_completions_dict = {}
+    cnt = 0
+    cnt_limit = 4
+    for line in data_sc:
+        # logging.info(f"line is {line}")
+        if line.strip() == "[" or line.strip() == "]":
+            logging.info("Skipping line")
+            continue
+        line_dict = eval(line)[0]
+        # logging.info(f"line_dict is {line_dict}")
+        prompt = line_dict["prompt"]
+        chosen = line_dict["chosen"]
+        rejected = line_dict["rejected"]
+        if prompt not in sc_completions_dict:
+            sc_completions_dict[prompt] = {"chosen": [], "rejected": []}
+        sc_completions_dict[prompt]["chosen"].append(chosen)
+        sc_completions_dict[prompt]["rejected"].append(rejected)
+        cnt += 1
+        if cnt >= cnt_limit:
+            break
+
+    logging.info(f"sc_completions_dict is {sc_completions_dict}")
     logging.info("Datasets built")
 
 
