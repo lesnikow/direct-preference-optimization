64860ab95b6feab6811327e860e074c2c8a97674
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2024-10-08 12:55:54 -0700

    Refactor build_datasets.py to make more general and reusable

--- build_datasets.py
+++ build_datasets.py
@@ -11,7 +11,15 @@ import sys
 import time
 
 
-def build_maj_data():
+def remove_im_start_end_tags(data):
+    """Remove the start and end tags from the data"""
+
+    data = str.replace(data, "<|im_start|>", "")
+    data = str.replace(data, "<|im_end|>", "")
+    return data
+
+
+def build_maj_data(fp_maj_all):
     """Build the majority datasets."""
 
     with open(fp_maj_all, "r") as f:
@@ -19,47 +27,67 @@ def build_maj_data():
         logging.info(f"Loaded {len(data_maj)} maj comments")
 
 
-def main():
-    """Main function"""
-
-    logging.info("Building datasets")
-
-    fp_maj_all = "/home/adam/data/reddit_data_v2/reddit_maj_data_for_DCPO_v2_all.json"
-    fp_sc_all = "/home/adam/data/reddit_data_v2/reddit_sc_data_for_DCPO_v2_all.json"
-
-    # Load the data, fp is actually a python lis of dictionaries.
+def build_completions(fp_sc_all, replace_im_start_end_tags=True):
+    """Build the completions dictionary."""
 
     with open(fp_sc_all, "r") as f:
         data_sc = f.readlines()
         logging.info(f"Loaded {len(data_sc)} sc comments")
 
-    # Build dict of completions, grouped by same prompt.
-    # Sample dictionary is form:
-    # {"prompt": "Prompt text ... ", "chosen": "Chosen completion text... ", "rejected":
-    # "rejected completion text..."}
-
-    sc_completions_dict = {}
+    completions = {}
     cnt = 0
     cnt_limit = 4
     for line in data_sc:
-        # logging.info(f"line is {line}")
         if line.strip() == "[" or line.strip() == "]":
             logging.info("Skipping line")
             continue
         line_dict = eval(line)[0]
-        # logging.info(f"line_dict is {line_dict}")
-        prompt = line_dict["prompt"]
-        chosen = line_dict["chosen"]
-        rejected = line_dict["rejected"]
-        if prompt not in sc_completions_dict:
-            sc_completions_dict[prompt] = {"chosen": [], "rejected": []}
-        sc_completions_dict[prompt]["chosen"].append(chosen)
-        sc_completions_dict[prompt]["rejected"].append(rejected)
+
+        prompt, chosen, rejected = (
+            line_dict["prompt"],
+            line_dict["chosen"],
+            line_dict["rejected"],
+        )
+
+        if replace_im_start_end_tags:
+            prompt, chosen, rejected = (
+                remove_im_start_end_tags(prompt),
+                remove_im_start_end_tags(chosen),
+                remove_im_start_end_tags(rejected),
+            )
+
+        if prompt not in completions:
+            completions[prompt] = {"chosen": [], "rejected": []}
+        completions[prompt]["chosen"].append(chosen)
+        completions[prompt]["rejected"].append(rejected)
+
         cnt += 1
         if cnt >= cnt_limit:
             break
 
-    logging.info(f"sc_completions_dict is {sc_completions_dict}")
+    for prompt in completions.keys():
+        logging.info(f"Prompt is:\n{prompt}")
+        logging.info(f"Chosen completions are:\n{completions[prompt]['chosen']}")
+        logging.info(f"Rejected completions are:\n{completions[prompt]['rejected']}")
+
+    return completions
+
+
+def write_out_dataset():
+    pass
+
+
+def main():
+    """Main function"""
+
+    logging.info("Building datasets")
+
+    fp_sc_all = "/home/adam/data/reddit_data_v2/reddit_sc_data_for_DCPO_v2_all.json"
+    fp_maj_all = "/home/adam/data/reddit_data_v2/reddit_maj_data_for_DCPO_v2_all.json"
+
+    build_completions(fp_sc_all)
+    # build_completions(fp_maj_all)
+
     logging.info("Datasets built")
 
 
